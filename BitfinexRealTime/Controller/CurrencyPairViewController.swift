cc9f598fc83ff40d0c7f4b3b0a4ca26aa7999a50
Ferdinando Messina <ferdinando.messina@gmail.com>
2018-05-03 16:17:11 +0200

    Introduce first version of CurrencyPairViewController

---
+++ BitfinexRealTime/Controller/CurrencyPairViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CurrencyPairViewController.swift
+//  BitfinexRealTime
+//
+//  Created by Ferdinando Messina on 03.05.18.
+//  Copyright © 2018 Ferdinando Messina. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyPairViewController: UIViewController {
+
+    var currencyPair: CurrencyPair!
+    
+    static func storyboardSegueIdentifier() -> String {
+        return "currencyPairSelectedSegue"
+    }
+    
+    func inject(currencyPair aCurrencyPair: CurrencyPair) {
+        currencyPair = aCurrencyPair
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = currencyPair.readableName
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
